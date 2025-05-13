efc9bfe85d1825f9f68687e7754365df2a918424
Stephan Fuhrmann <s@sfuhrm.de>
2025-05-13 22:11:11 +0200

    Rework to file based operation

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,11 @@
 #!/bin/sh -l
 
-echo "Reading artifact ${INPUT_ARTIFACT_VAR}"
+# https://docs.github.com/en/actions/sharing-automations/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
 
-##
-## https://docs.github.com/en/rest/actions/artifacts?apiVersion=2022-11-28
-##
-gh api \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/${REPO_OWNER_VAR}/actions/artifacts/${INPUT_ARTIFACT_VAR} > input_artifact.json
+# import GPG key
+gpg --batch --import "/github/workspace/${GPG_KEY_VAR}"
 
-ARCHIVE_URL=$(jq -r .archive_download_url < input_artifact.json)
-
-echo "Archive URL: ${ARCHIVE_URL}"
-
-gh api \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  /repos/${REPO_OWNER_VAR}/actions/artifacts/${INPUT_ARTIFACT_VAR}/zip > input.zip
-
-echo "Listing"
-zip -t input.zip
+# find all files
+find "/github/workspace/${INPUT_PATH_VAR}" -type f | while read file; do
+    gpg --pinentry-mode=loopback --passphrase "$1" --armor --detach-sign hosts
+done
