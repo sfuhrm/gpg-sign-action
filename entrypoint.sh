15e224264e10fa0d4430e60ddeb9056651bdecfd
Stephan Fuhrmann <s@sfuhrm.de>
2025-05-18 21:41:26 +0200

    debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,23 @@
 #!/bin/sh -le
 
+
 DEBUG=1
+debug()
+{
+    if [ $DEBUG -eq 1 ]; then
+        echo $@
+    fi
+}
 
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
 
 # start GPG agent
-[ $DEBUG == 1] || echo "Starting agent"
+debug "Starting agent"
 gpg-agent --daemon --batch --disable-scdaemon
 
-[ $DEBUG == 1] || echo "Counting blocklines"
+debug "Counting blocklines"
 BLOCKLINES=$(echo "${GPG_KEY_VAR}" | grep -c "PRIVATE KEY BLOCK")
-[ $DEBUG == 1] || echo "Blocklines: $BLOCKLINES"
+debug "Blocklines: $BLOCKLINES"
 
 if [ $BLOCKLINES != "2" ]; then
     echo "The GPG key does not contain two PRIVATE KEY BLOCk markers, but ${BLOCKLINES}."
@@ -19,12 +26,12 @@ if [ $BLOCKLINES != "2" ]; then
 fi
 
 # check and import GPG key
-[ $DEBUG == 1] || echo "Import key"
+debug "Import key"
 echo "${GPG_KEY_VAR}"  | gpg --batch --import
-[ $DEBUG == 1] || echo "Imported key"
+debug "Imported key"
 
 PUBKEYLINES=$(gpg --list-keys|grep -c -E "^pub")
-[ $DEBUG == 1] || echo "Pubkeys: $PUBKEYLINES"
+debug "Pubkeys: $PUBKEYLINES"
 if [ $PUBKEYLINES == "0" ]; then
     echo "The GPG import failed, expected at least 1, but got ${PUBKEYLINES} pubkeys."
     exit 2
@@ -32,7 +39,7 @@ fi
 
 # find all files
 find "/github/workspace/${INPUT_PATH_VAR}" -type f | while read file; do
-    [ $DEBUG == 1] || echo "Signing file: $file"
+    debug "Signing file: $file"
     gpg --pinentry-mode=loopback --passphrase "$1" --armor --detach-sign "$file"
 done
 
