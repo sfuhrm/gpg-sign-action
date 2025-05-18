fc72b7762697eca90cf5ffdbc54db373b28cc42c
Stephan Fuhrmann <s@sfuhrm.de>
2025-05-18 19:18:43 +0200

    Remove checking Work with stdin

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,7 @@
 gpg-agent --daemon --batch --disable-scdaemon
 
 # check and import GPG key
-KEY=/tmp/gpgkey
-
-echo "${GPG_KEY_VAR}" > $KEY
-
-grep "BEGIN PGP PRIVATE KEY BLOCK-----" $KEY || (echo "Key needs to begin with: -----BEGIN PGP PRIVATE KEY BLOCK-----" >&2; exit 2)
-grep "END PGP PRIVATE KEY BLOCK-----" $KEY || (echo "Key needs to end with: -----END PGP PRIVATE KEY BLOCK-----" >&2; exit 2)
-
-chmod 0700 $KEY
-gpg --batch --import $KEY
-rm -f $KEY
+echo "${GPG_KEY_VAR}"  | gpg --batch --import
 
 # find all files
 find "/github/workspace/${INPUT_PATH_VAR}" -type f | while read file; do
