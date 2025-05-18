e0e39480aa80630c80d5b1e9b700892edc226491
Stephan Fuhrmann <s@sfuhrm.de>
2025-05-18 21:32:01 +0200

    New checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,22 @@
 # start GPG agent
 gpg-agent --daemon --batch --disable-scdaemon
 
+BLOCKLINES=$(echo "${GPG_KEY_VAR}" | grep -c "PRIVATE KEY BLOCK" /tmp/priv.asc)
+
+if [ $BLOCKLINES != "2" ]; then
+    echo "The GPG key does not contain two PRIVATE KEY BLOCk markers, but ${BLOCKLINES}."
+    echo "Please ensure your key is in ASCII ARMOR (--armor) format."
+    exit 1
+fi
+
 # check and import GPG key
 echo "${GPG_KEY_VAR}"  | gpg --batch --import
+
+PUBKEYLINES=$(gpg --list-keys|grep pub|wc -l)
+if [ $PUBKEYLINES == "0" ]; then
+    echo "The GPG import failed, expected at least 1, but got ${PUBKEYLINES} pubkeys."
+    exit 2
+fi
 
 # find all files
 find "/github/workspace/${INPUT_PATH_VAR}" -type f | while read file; do
