4dcf9923c3fb95d3d3c2e98efc767f52e8162c21
Stephan Fuhrmann <s@sfuhrm.de>
2025-05-18 18:53:55 +0200

    Fix error handling

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh -l -e
 
 # https://docs.github.com/en/actions/sharing-automations/creating-actions/creating-a-docker-container-action#accessing-files-created-by-a-container-action
 
@@ -10,8 +10,8 @@ KEY=/tmp/gpgkey
 
 echo "${GPG_KEY_VAR}" > $KEY
 
-grep "-----BEGIN PGP PRIVATE KEY BLOCK-----" $KEY || (echo "Key needs to begin with: -----BEGIN PGP PRIVATE KEY BLOCK-----" >&2; exit 2)
-grep "-----END PGP PRIVATE KEY BLOCK-----" $KEY || (echo "Key needs to end with: -----END PGP PRIVATE KEY BLOCK-----" >&2; exit 2)
+grep "BEGIN PGP PRIVATE KEY BLOCK-----" $KEY || echo "Key needs to begin with: -----BEGIN PGP PRIVATE KEY BLOCK-----" >&2; exit 2)
+grep "END PGP PRIVATE KEY BLOCK-----" $KEY || (echo "Key needs to end with: -----END PGP PRIVATE KEY BLOCK-----" >&2; exit 2)
 
 chmod 0700 $KEY
 gpg --batch --import $KEY
